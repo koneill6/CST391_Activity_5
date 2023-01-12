e1611b13c5eeb6ad9859cb12f4783888a796a6c6
kody-oneill <kody.oneill@yahoo.com>
2023-01-11 21:44:35 -0600

    Part 3 complete

--- music/src/App.js
+++ music/src/App.js
@@ -1,20 +1,55 @@
 import React from 'react';
 import Card from './Card';
+import './App.css';
 
-const App = () => {
-    return (
-        <div>
-            <h1>Music I like</h1>
-            <Card 
-                imageurl="//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg" 
-                albumtitle="Goodbye Yellow Brick Road" 
-                albumdescription="Goodbye Yellow Brick Road is the seventh studio album by English singer-songwriter Elton John, first released on 5 October 1973 as a double LP"
-                buttontext="OK"
-            />
-            <Card />
-            <Card />
-        </div>
-    )
+class App extends React.Component {
+
+    state = { albumList: [
+        {
+        "id":0,
+        "title":"Goodbye Yellow Brick Road",
+        "description":"Goodbye Yellow Brick Road is the seventh studio album by English singer-songwriter Elton John, first released on 5 October 1973 as a double LP",
+        "year": 1973,
+        "image":"//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg"
+        },
+        {
+        "id":1,
+        "title":"Goodbye Yellow Brick Road",
+        "description":"Goodbye Yellow Brick Road is the seventh studio album by English singer-songwriter Elton John, first released on 5 October 1973 as a double LP",
+        "year": 1973,
+        "image":"//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg"
+        },
+        {
+        "id":2,
+        "title":"Goodbye Yellow Brick Road",
+        "description":"Goodbye Yellow Brick Road is the seventh studio album by English singer-songwriter Elton John, first released on 5 October 1973 as a double LP",
+        "year": 1973,
+        "image":"//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg"
+        },
+    ]};
+
+    renderedList = () => {
+        return this.state.albumList.map(
+            (album) => {
+                return(
+                    <Card 
+                    imageurl={album.image} 
+                    albumtitle={album.title}
+                    albumdescription={album.description}
+                    buttontext="OK"
+                    />
+                )
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                {this.renderedList()}
+            </div>
+        )
+    }
 }
 
 export default App;
